de71ca1184ae4447d6ac4271787bbf0d59d7788a
sunnengsen <sunnengsen@gmail.com>
2024-05-11 17:04:23 +0700

    Add viewBinding and update UI elements

--- app/src/main/java/com/example/bontub/MainActivity.java
+++ app/src/main/java/com/example/bontub/MainActivity.java
@@ -1,12 +1,46 @@
 package com.example.bontub;
-import android.app.Activity;
-import android.os.Bundle;
 
-public class MainActivity extends Activity {
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import com.example.bontub.databinding.ActivityMainBinding;
+
+
+public class MainActivity extends AppCompatActivity {
+    ActivityMainBinding binding;
+    private Button button;
+    @SuppressLint({"NonConstantResourceId", "WrongViewCast"})
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_profile);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        binding.bottomNav.setOnNavigationItemSelectedListener(item -> {
+            Fragment selectedFragment = null;
+            if (item.getItemId() == R.id.home) {
+                selectedFragment = new HomeFragment();
+            } else if (item.getItemId() == R.id.accessory) {
+                selectedFragment = new AccessoryFragment();
+            } else if (item.getItemId() == R.id.reserve) {
+                selectedFragment = new ReserveFragment();
+            } else if (item.getItemId() == R.id.profile) {
+                selectedFragment = new ProfileFragment();
+            }
+            assert selectedFragment != null;
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, selectedFragment).commit();
+            return true;
+        });
+        // Manually selecting the home item
+        binding.bottomNav.setSelectedItemId(R.id.home);
 
     }
+
 }
